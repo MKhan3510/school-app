5eef083a6c7f57c9b446f66b249e8fccd3ef9dc6
Mohamed <mrkhan3510@gmail.com>
2021-05-21 17:38:27 +0000

    home commit

---
+++ Components/Contact.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+ 
+class Stuff extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Trips That The Sep went on</h2>
+        <p>Being in this program allows you to go on different trips. Some of these trips incudes going to the Tech fair at the Jacob Javits Center, Going to VIA to learn about how coding and software engineering works and much more</p>
+        <ol>
+          <li><img src="https://paganresearch.io/images/Via_(Carpooling).jpg" alt="The Monk Diamond"/></li>
+          <li><img src="https://techsponyc.com/wp-content/uploads/2018/12/who-attends.jpg" alt="The Monk Diamond" width="500" height="300"/></li>
+        </ol>
+      </div>
+    );
+  }
+}
+ 
+export default Stuff;
