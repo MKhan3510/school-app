db3016fc9c2e150a90232f7add742dc5617d8e8f
Mohamed <mrkhan3510@gmail.com>
2021-05-21 17:37:37 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
-
+import Navbar from './Components/Navbar'
+import './Bootstrap.css'
+import Home from './Components/Home'
 function App() {
   return (
+   
     <div className="App">
-      <h1>Hello React</h1>
+      <Navbar/>
+ 
+      
     </div>
   );
 }
---
+++ src/Components/Stuff.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+ 
+class Stuff extends Component {
+  render() {
+    return (
+      <div>
+        <h2>What do you do in SEP</h2>
+        <p>Here in the software engineering pathway you will learn different coding platform such as scratch in 9th grade, p5 in 10th grade, a combination of python, html, and algorithms in 11th, and finally in your 12th grade year you will learn more about web development and javascript</p>
+        <ol>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ol>
+      </div>
+    );
+  }
+}
+ 
+export default Stuff;
